58d76addcf995e6a65db1ce988414f2a7cc07a66
PhilCAEUser <rbyETE4Ceu2iA4Z>
2021-02-22 10:17:13 +0000

    Init

---
+++ db/10002_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 10002, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab.
+--
+CREATE TABLE caeschema.tab (
+  tes INTEGER ,
+CONSTRAINT tes_PK PRIMARY KEY (tes)
+ 
+);
+
+
+
